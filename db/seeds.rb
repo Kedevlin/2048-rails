624cb69368730f97882158ce82bc6a08bd11f58d
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-08 16:27:47 -0800

    added user seeds with high scores:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+users = [{uid: 156, high_score: 1}, {uid: 324, high_score: 342}, {uid: 1, high_score: 232}, {uid: 234, high_score: 231}, {uid: 2, high_score: 213}]
+
+users.each do |user|
+  User.new(user)
+end
