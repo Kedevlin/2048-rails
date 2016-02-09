da9bb8dec80fc3c0831016eade2d46ab107c8232
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-08 16:21:05 -0800

    added high score column to users

---
+++ db/migrate/20160209002029_add_high_score.rb
@@ -0,0 +1,5 @@
+class AddHighScore < ActiveRecord::Migration
+  def change
+    add_column(:users, :high_score, :integer)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208233912) do
+ActiveRecord::Schema.define(version: 20160209002029) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "score"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160208233912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "uid"
+    t.integer  "high_score"
   end
 
 end
