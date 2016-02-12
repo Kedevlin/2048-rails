fde54981be7cfa411fb232ef55abe77a7f59cf1a
Kelly <kelly_devlin@alumni.brown.edu>
2016-02-12 09:45:14 -0800

    add error handling to site_controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,12 +6,16 @@ before_action :require_login, only: :index
 
   def delete_game
     Game.destroy(params[:id])
-    render :json => []
+    render :json => [], :status => :ok
   end
 
   def leaderboard
     @leaders = User.order(high_score: :desc).limit(3)
-    render :json  => @leaders, :only => [:uid, :high_score]
+    if @leaders
+      render :json  => @leaders, :only => [:uid, :high_score]
+    else
+      render :json => [], :status => :no_content
+    end
   end
 
 end
